e767924112d011d6e0a3997dc35d39978c4ef891
James Eddy <james.a.eddy@gmail.com>
2021-07-21 17:01:26 +0000

    fix pkg name in app/module scripts

--- R/app_config.R
+++ R/app_config.R
@@ -5,7 +5,7 @@
 #' 
 #' @noRd
 app_sys <- function(...){
-  system.file(..., package = "includeshinytemplate")
+  system.file(..., package = "pubincluder")
 }
 
 
--- R/app_ui.R
+++ R/app_ui.R
@@ -35,7 +35,7 @@ golem_add_external_resources <- function(){
     favicon(),
     bundle_resources(
       path = app_sys('app/www'),
-      app_title = 'includeshinytemplate'
+      app_title = 'pubincluder'
     )
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert() 
