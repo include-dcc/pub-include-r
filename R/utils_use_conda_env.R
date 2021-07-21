1d18714fc681f300c2cb4a96efda492b4cc80781
James Eddy <james.a.eddy@gmail.com>
2021-07-21 09:30:04 -0700

    add scripts for setting conda env (not used yet)

---
+++ R/utils_use_conda_env.R
@@ -0,0 +1,8 @@
+#' use_conda_env 
+#'
+#' @description A utils function
+#'
+#' @return The return value, if any, from executing the utility.
+#'
+#' @noRd
+reticulate::use_condaenv("synapseclient", required = TRUE)
